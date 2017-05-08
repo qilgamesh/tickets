27f30ae03d4d869467bc7c4e68aba424934dd301
Andrey Semenyuk <qilgamesh@qilnet.ru>
2017-05-09 01:17:55 +0500

    Обработчик базы данных: добавлены методы сохранения моделей

--- src/handlers/DbHandler.java
+++ src/handlers/DbHandler.java
@@ -1,16 +1,18 @@
 package handlers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import model.Job;
+import model.Ticket;
 import org.sqlite.JDBC;
 import utils.LogUtils;
 
 import java.io.File;
 import java.sql.*;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,6 +22,7 @@ import java.util.logging.Logger;
  * Created by Andrey Semenyuk on 2017.
  */
 public class DbHandler {
+
     // Константа, в которой хранится адрес подключения
     private static final String CON_STR = "jdbc:sqlite:db/tickets.sqlite";
     // Объект, в котором будет храниться соединение с БД
@@ -72,11 +75,11 @@ public class DbHandler {
         try {
             Statement statement = connection.createStatement();
             statement.execute(
-                    "CREATE TABLE IF NOT EXISTS 'jobs' (" +
+                    "CREATE TABLE IF NOT EXISTS 'job' (" +
                             "'id' INTEGER PRIMARY KEY AUTOINCREMENT," +
                             "'name' TEXT," +
                             "'state' TEXT," +
-                            "'execute_date' TIMESTAMP);"
+                            "'departure_date' TIMESTAMP);"
             );
             statement.execute(
                     "CREATE TABLE IF NOT EXISTS 'airplane' (" +
@@ -86,6 +89,15 @@ public class DbHandler {
                             "'places_near_window' INTEGER," +
                             "'long_legs_rows' TEXT);"
             );
+            statement.execute(
+                    "CREATE TABLE IF NOT EXISTS 'ticket' (" +
+                            "'id' INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            "'last_name' TEXT," +
+                            "'number' TEXT," +
+                            "'date' TEXT," +
+                            "'flight_number' TEXT," +
+                            "'job_id' INTEGER REFERENCES job (id));"
+            );
         } catch (SQLException ex) {
             logger.log(Level.SEVERE, "Failed to create tables: ", ex);
         }
@@ -94,57 +106,179 @@ public class DbHandler {
     /**
      * Получение активных заданий
      *
-     * @return List - список заданий или пустой список, если произошла ошибка
+     * @return List - список заданий
      */
     public List<Job> getActiveJobs() {
-        // для теста
-        return getActiveJobsTest();
-/*
-        try (Statement statement = this.connection.createStatement()) {
-            List<Job> jobs = new ArrayList<>();
-            ResultSet resultSet = statement.executeQuery("SELECT id, name, execute_date, state FROM main.jobs WHERE state = 'active';");
-
-            while (resultSet.next()) {
-                jobs.add(new Job(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name"),
-                        resultSet.getString("state"),
-                        resultSet.getString("execute_date"))
-                );
-            }
-
-            return jobs;
-        } catch (SQLException ex) {
-            logger.log(Level.SEVERE, "Failed to get jobs: ", ex);
-            return Collections.emptyList();
-        }*/
-    }
-
-    /**
-     * Получение активных заданий - тестовый
-     *
-     * @return List - список тестовых заданий
-     */
-    public List<Job> getActiveJobsTest() {
 
         List<Job> jobs = new ArrayList<>();
 
-        jobs.add(new Job(1, "test3", "ACTIVE", LocalDateTime.now().plusSeconds(3630).toString()));
-        jobs.add(new Job(3, "test1", "ACTIVE", LocalDateTime.now().plusSeconds(3610).toString()));
-        jobs.add(new Job(2, "test4", "ACTIVE", LocalDateTime.now().plusSeconds(3640).toString()));
-        jobs.add(new Job(4, "test2", "ACTIVE", LocalDateTime.now().plusSeconds(3620).toString()));
+        try (Statement statement = this.connection.createStatement()) {
+            // получяем только активные задания
+            // ResultSet rs = statement.executeQuery("SELECT id, name, execute_date, state FROM main.jobs WHERE state = 'active';");
+            // TODO для теста !! удалить после теста
+            ResultSet rs = statement.executeQuery("SELECT * FROM job;");
+            Random random = new Random();
+
+            while (rs.next()) {
+                Job job = new Job(rs.getInt("id"), rs.getString("name"), rs.getString("state"), rs.getString("departure_date"));
+                job.setTickets(getTicketByJobId(job.getId()));
+                // TODO для теста !! удалить после теста
+                int rnd = random.nextInt(30);
+                job.setDepartureDate(LocalDateTime.now().plusSeconds(3610 + rnd).toString());
+                job.setState("NEW");
+                job.setName("JOB#" + rnd);
+                jobs.add(job);
+            }
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Failed to get jobs: ", ex);
+        }
 
         return jobs;
     }
 
-    public void updateJobState(Job job) {
-        try (Statement statement = this.connection.createStatement()) {
+    /**
+     * Получение билетов по id заданию
+     *
+     * @param id задания
+     * @return Ticket
+     */
+    private ObservableList<Ticket> getTicketByJobId(int id) {
 
-//            statement.executeQuery("UPDATE jobs SET state id, name, execute_date, state FROM main.jobs WHERE state = 'active';");
-            logger.info("Job update state");
+        ObservableList<Ticket> tickets = FXCollections.observableArrayList();
+
+        try (Statement statement = this.connection.createStatement()) {
+            ResultSet rs = statement.executeQuery("SELECT * FROM ticket WHERE job_id = " + id);
+
+            while (rs.next()) {
+                tickets.add(new Ticket(rs.getInt("id"), rs.getString("last_name"), rs.getString("number"), rs.getString("date"), rs.getString("flight_number"), id));
+            }
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Failed to get tickets for job id= " + id, ex);
+        }
+
+        return tickets;
+    }
+
+    public Job getJob(int id) {
+
+        if (id == 0) {
+            return null;
+        }
+
+        try {
+            Statement statement = this.connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM job WHERE id = " + id);
+            rs.next();
+            logger.info("Result select job: " + rs.getRow());
+            return new Job(id, rs.getString(2), rs.getString(3), rs.getString(4));
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Failed to create statement", ex);
+        }
+
+        return null;
+    }
+
+    /**
+     * Сохранение или добавление нового задания в БД.
+     *
+     * @param job новое или изменённое задание
+     */
+    public synchronized void saveJob(Job job) {
+
+        Job savedJob = getJob(job.getId());
+        String smnt = "UPDATE job SET (name, state, departure_date) = (?, ?, ?) WHERE id = " + job.getId() + ";";
+
+        if (savedJob == null) {
+            smnt = "INSERT INTO job (name, state, departure_date) VALUES (?, ?, ?);";
+            logger.info("Save new job with name: " + job.getName());
+        } else {
+            logger.info("Update saved job with name: " + savedJob.getName());
+        }
+
+        try {
+            PreparedStatement statement = this.connection.prepareStatement(smnt);
+            statement.setString(1, job.getName());
+            statement.setString(2, job.getState());
+            statement.setString(3, String.valueOf(job.getDepartureDateTimestamp()));
+
+            if (statement.executeUpdate() == 0) {
+                logger.warning("Save job error");
+            }
+
+            if (savedJob == null) {
+                int id = statement.getGeneratedKeys().getInt(1);
+                logger.info("New job successful saved (id: " + id + ")");
+                job.getTickets().forEach(ticket -> ticket.setJobId(id));
+            } else {
+                logger.info("Job successful updated (id=" + job.getId() + ")");
+            }
+
+            job.getTickets().forEach(this::saveTicket);
 
         } catch (SQLException ex) {
-            logger.log(Level.SEVERE, "Failed to update jobs ", ex);
+            logger.log(Level.SEVERE, "Failed to create statement", ex);
+        }
+    }
+
+    public Ticket getTicket(int id) {
+
+        if (id == 0) {
+            return null;
+        }
+
+        try {
+            Statement statement = this.connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM ticket WHERE id = " + id);
+            rs.next();
+
+            return new Ticket(id, rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6));
+
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Failed to create statement", ex);
+        }
+
+        return null;
+    }
+
+    /**
+     * Сохранение или добавление нового билета в БД.
+     *
+     * @param ticket новый или изменённый билет
+     */
+    public synchronized void saveTicket(Ticket ticket) {
+
+        String smnt = "UPDATE ticket SET (last_name, number, date, flight_number, job_id) = (?, ?, ?, ?, ?) WHERE id = " + ticket.getId() + ";";
+
+        Ticket savedTicket = getTicket(ticket.getId());
+
+        if (savedTicket == null) {
+            smnt = "INSERT INTO ticket (last_name, number, date, flight_number, job_id) VALUES (?, ?, ?, ?, ?);";
+            logger.info("Save new ticket for job id: " + ticket.getJobId());
+        } else {
+            logger.info("Update saved ticket for job id: " + savedTicket.getJobId());
+        }
+
+        try {
+            PreparedStatement statement = this.connection.prepareStatement(smnt);
+            statement.setString(1, ticket.getLastName());
+            statement.setString(2, ticket.getNumber());
+            statement.setString(3, ticket.getDate());
+            statement.setString(4, ticket.getFlightNumber());
+            statement.setInt(5, ticket.getJobId());
+
+            if (statement.executeUpdate() == 0) {
+                logger.warning("Save ticket error");
+                return;
+            }
+
+            if (savedTicket == null) {
+                int id = statement.getGeneratedKeys().getInt(1);
+                logger.info("New ticket successful saved (id: " + id + ")");
+            } else {
+                logger.info("Ticket successful updated (id=" + ticket.getId() + ")");
+            }
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Failed to create statement", ex);
         }
     }
 }
